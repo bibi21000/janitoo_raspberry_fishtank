f54fc732b366612c022939d8c07fd49ba865368e
bibi21000 <bibi21000@gmail.com>
2016-01-26 19:47:50 +0100

    Split camera

--- src/janitoo_raspberry/thread_basic.py
+++ src/janitoo_raspberry/thread_basic.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+"""The Raspberry camera worker
 
-"""Unittests for Janitoo-Roomba Server.
+Installation :
+
+.. code-block:: bash
+
+    sudo apt-get install python-pycamera
+
 """
+
 __license__ = """
     This file is part of Janitoo.
 
@@ -23,36 +30,42 @@ __author__ = 'Sébastien GALLET aka bibi21000'
 __email__ = 'bibi21000@gmail.com'
 __copyright__ = "Copyright © 2013-2014-2015 Sébastien GALLET aka bibi21000"
 
-import sys, os
-import time, datetime
-import unittest
-import threading
+# Set default logging handler to avoid "No handler found" warnings.
 import logging
-from pkg_resources import iter_entry_points
+logger = logging.getLogger(__name__)
+import os, sys
+import threading
+import time
+import datetime
+import socket
+from janitoo.thread import JNTBusThread
+from janitoo.bus import JNTBus
+from janitoo.component import JNTComponent
+from janitoo.thread import BaseThread
+from janitoo.options import get_option_autostart
 
-from janitoo_nosetests.server import JNTTServer, JNTTServerCommon
-from janitoo_nosetests.thread import JNTTThread, JNTTThreadCommon
-from janitoo_nosetests.component import JNTTComponent, JNTTComponentCommon
-
-from janitoo.utils import json_dumps, json_loads
-from janitoo.utils import HADD_SEP, HADD
-from janitoo.utils import TOPIC_HEARTBEAT
-from janitoo.utils import TOPIC_NODES, TOPIC_NODES_REPLY, TOPIC_NODES_REQUEST
-from janitoo.utils import TOPIC_BROADCAST_REPLY, TOPIC_BROADCAST_REQUEST
-from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_SYSTEM, TOPIC_VALUES_BASIC
-
-from janitoo_raspberry.thread_camera import CameraThread
 ##############################################################
 #Check that we are in sync with the official command classes
 #Must be implemented for non-regression
 from janitoo.classes import COMMAND_DESC
 
-COMMAND_DISCOVERY = 0x5000
+COMMAND_CONTROLLER = 0x1050
 
-assert(COMMAND_DESC[COMMAND_DISCOVERY] == 'COMMAND_DISCOVERY')
+assert(COMMAND_DESC[COMMAND_CONTROLLER] == 'COMMAND_CONTROLLER')
 ##############################################################
 
-class TesCameraThread(JNTTThread, JNTTThreadCommon):
-    """Test the thread
+def make_thread(options):
+    if get_option_autostart(options, 'rpibasic') == True:
+        return CameraThread(options)
+    else:
+        return None
+
+class CameraThread(JNTBusThread):
+    """The basic thread
+
     """
-    thread_name = "picamera"
+    def init_bus(self):
+        """Build the bus
+        """
+        self.section = 'rpibasic'
+        self.bus = JNTBus(options=self.options, oid=self.section, product_name="Raspberry basic controller")
