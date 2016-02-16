e3f243dc95a44b9a3ad74f6dbf5d25ca9f3ccc9a
bibi21000 <bibi21000@gmail.com>
2016-02-16 01:02:25 +0100

    Updates for tests

--- tests/test_thread.py
+++ tests/test_thread.py
@@ -69,3 +69,48 @@ class TestFishtankThread(JNTTThreadRun, JNTTThreadRunCommon):
         #~ self.assertDir("/tmp/janitoo_test/home/public/generic/css")
         #~ self.assertDir("/tmp/janitoo_test/home/public/generic/images")
         #~ self.assertDir("/tmp/janitoo_test/home/public/generic/doc")
+
+    def test_102_check_values(self):
+        self.thread.start()
+        timeout = 120
+        i = 0
+        while i< timeout and not self.thread.nodeman.is_started:
+            time.sleep(1)
+            i += 1
+            #~ print self.thread.nodeman.state
+        time.sleep(15)
+        self.thread.bus.on_check()
+        time.sleep(10)
+        print self.thread.bus.nodeman.nodes
+        print self.thread.bus.nodeman.find_node('ambiance')
+        print self.thread.bus.nodeman.find_node('surftemp')
+        print self.thread.bus.nodeman.find_node('deeptemp')
+        print self.thread.bus.nodeman.find_node('moon')
+        print self.thread.bus.nodeman.find_node('tide')
+        print self.thread.bus.nodeman.find_node('motortide')
+        print self.thread.bus.nodeman.find_node('sun')
+        print self.thread.bus.nodeman.find_node('ledmoon')
+        print self.thread.bus.nodeman.find_node('ledsun')
+        print self.thread.bus.nodeman.find_node('thermostat')
+        print self.thread.bus.nodeman.find_node('switch_fullsun')
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('ambiance'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('ambiance','temperature'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('ambiance','humidity'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('surftemp'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('surftemp','temperature'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('deeptemp'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('deeptemp','temperature'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('moon'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('moon','factor_now'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('tide'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('motortide'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('sun'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('sun','factor_now'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('ledmoon'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('ledmoon','level'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('ledmoon','max_level'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('ledsun'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('ledsun','level'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_value('ledsun','max_level'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('thermostat'))
+        self.assertNotEqual(None, self.thread.bus.nodeman.find_node('switch_fullsun'))
